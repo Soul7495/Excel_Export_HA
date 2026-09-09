ab3e0879994b1b58c530e0ee41c6a4ec9863be34
Soul7495 <michi72rr@gmail.com>
2026-09-09 14:36:20 +0200

    Add files via upload

---
+++ ha-device-entity-xlsx-export/custom_components/device_entity_xlsx_export/const.py
@@ -0,0 +1,13 @@
+"""Constants for Device Entity XLSX Export."""
+
+DOMAIN = "device_entity_xlsx_export"
+SERVICE_EXPORT_DEVICE = "export_device"
+EXPORT_DIR = "entity_exports"
+
+ATTR_DEVICE_ID = "device_id"
+ATTR_FILENAME = "filename"
+ATTR_INCLUDE_DISABLED = "include_disabled"
+ATTR_INCLUDE_ATTRIBUTES = "include_attributes"
+
+DEFAULT_INCLUDE_DISABLED = True
+DEFAULT_INCLUDE_ATTRIBUTES = True
